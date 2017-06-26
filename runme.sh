04f0a6934884fb8b066bc6d2747e332200d92e84
Konstantin Odnoralov <hostmit@gmail.com>
2017-06-26 19:18:22 +0300

    v1

--- runme.sh
+++
@@ -1,7 +0,0 @@
-docker build . -t xupnpd/xupnpd
-
-
-
-docker run -d --name=xupnpd --net=host -v /docker/xupnpd/config:/xupnpd/config/ xupnpd/xupnpd
-
-docker run -it --name=xupnpd --net=host -v /docker/xupnpd/config:/xupnpd/config/ xupnpd3/xupnpd3
